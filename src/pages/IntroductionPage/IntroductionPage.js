c57e69a4eec2db40e07f7968a03e2b2709cca92a
Sillas Victor <Sillas_2@hotmail.com>
2022-01-17 17:00:58 +0000

    add correct redirect for introduction page

--- src/pages/IntroductionPage/IntroductionPage.js
+++ src/pages/IntroductionPage/IntroductionPage.js
@@ -11,7 +11,7 @@ function IntroductionPage() {
         That’s why we created Fragile. All you share with Fragile, will be
         anonymized and <b>won’t be shared with 3rd parties.</b>{" "}
       </p>
-      <Link to="/episode/61b9e89384c5217a3f53ce6c">
+      <Link to="/episode/61b9c5dd84c5217a3f53ce4b">
         <button className="btn-signup">Got it</button>
       </Link>
     </>
